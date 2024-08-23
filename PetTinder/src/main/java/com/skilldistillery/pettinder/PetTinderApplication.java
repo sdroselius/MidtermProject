6ae06ebd4e5fc819d08e92b69fd7b56b650616ff
Rob Roselius <roselius@skilldistillery.com>
2024-08-23 10:15:32 -0600

    Configure JPA project dependency

---
+++ PetTinder/src/main/java/com/skilldistillery/pettinder/PetTinderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pettinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTinderApplication.class, args);
+	}
+
+}
---
+++ PetTinder/src/test/java/com/skilldistillery/pettinder/PetTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pettinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
