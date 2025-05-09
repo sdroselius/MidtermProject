1c556407c11015d8af41f49204ca49c1381f07a8
Rob Roselius <roselius@skilldistillery.com>
2025-05-09 11:24:16 -0600

    Map and test User entity

--- JPAZooTinder/src/main/java/com/skilldistillery/zootinder/entities/User.java
+++ JPAZooTinder/src/main/java/com/skilldistillery/zootinder/entities/User.java
@@ -1,5 +1,92 @@
 package com.skilldistillery.zootinder.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String username;
+	private String password;
+
+	private Boolean enabled;
+	private String role;
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=").append(id).append(", username=").append(username).append(", password=")
+				.append(password).append(", enabled=").append(enabled).append(", role=").append(role).append("]");
+		return builder.toString();
+	}
 
 }
--- JPAZooTinder/src/test/java/com/skilldistillery/zootinder/entities/UserTest.java
+++ JPAZooTinder/src/test/java/com/skilldistillery/zootinder/entities/UserTest.java
@@ -8,27 +8,42 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAZooTinder");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
