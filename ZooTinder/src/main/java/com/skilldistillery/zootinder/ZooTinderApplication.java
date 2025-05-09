d2c7dbd6028cea8a3b9d242bade0e4beb6665b84
Rob Roselius <roselius@skilldistillery.com>
2025-05-09 10:11:56 -0600

    Create Spring Boot MVC project, configure JPA project dependency

---
+++ ZooTinder/src/main/java/com/skilldistillery/zootinder/ZooTinderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.zootinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooTinderApplication.class, args);
+	}
+
+}
---
+++ ZooTinder/src/test/java/com/skilldistillery/zootinder/ZooTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.zootinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
